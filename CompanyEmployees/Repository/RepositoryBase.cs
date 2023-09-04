d585b2863c545405275f18966aa620827ed450a7
namknf <9093264418es@gmail.com>
2023-09-04 20:26:15 +0300

    add repo project

---
+++ CompanyEmployees/Repository/RepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected DataContext DataContext;
+
+        public RepositoryBase(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? DataContext.Set<T>().AsNoTracking() : DataContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ? DataContext.Set<T>().Where(expression).AsNoTracking() : DataContext.Set<T>().Where(expression);
+
+        public void Create(T entity) => DataContext.Set<T>().Add(entity);
+
+        public void Update(T entity) => DataContext.Set<T>().Update(entity);
+
+        public void Delete(T entity) => DataContext.Set<T>().Remove(entity);
+    }
+}
