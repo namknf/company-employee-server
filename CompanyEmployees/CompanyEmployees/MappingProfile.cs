eb4134d0f45674c3bd5f06e92033747e6fb98cb7
namknf <9093264418es@gmail.com>
2023-09-05 20:41:49 +0300

    add controller method for company creation

--- CompanyEmployees/CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/CompanyEmployees/MappingProfile.cs
@@ -19,6 +19,8 @@ namespace CompanyEmployees
             CreateMap<Address, AddressDto>()
                 .ForMember(c => c.FullAddress,
                 opt => opt.MapFrom(x => string.Join(' ', x.Country, x.City, x.Region)));
+
+            CreateMap<CompanyForCreationDto, Company>();
         }
     }
 }
---
+++ CompanyEmployees/Entities/DataTransferObjects/CompanyForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Models;
+
+namespace Entities.DataTransferObjects
+{
+    public class CompanyForCreationDto
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
